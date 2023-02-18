02e5820f8f6e91938264646e01f8a520ba7f3e3f
IyeasinI <124849241+IyeasinI@users.noreply.github.com>
2023-02-18 18:33:54 +0600

    Add files via upload

--- java/index.js
+++ java/index.js
@@ -1,29 +1,89 @@
 var hamburger = document.querySelector(".hamburger");
-var bar = document.querySelector(".bar");
-var navLink = document.querySelector(".nav_link");
+var hamburgerMenu = document.querySelector(".hamburger_menu");
+hamburger.addEventListener("click", mobileMenu);
 
-hamburger.addEventListener("click", ()=>{
+function mobileMenu() {
     hamburger.classList.toggle("active");
-  navLink.classList.toggle("height_auto");
-});
-
-
-const faqs = document.querySelectorAll(".faq"); 
-
-faqs.forEach( (faq) => {
-  faq.addEventListener("click",() =>{
-  faq.classList.toggle("achsd");
-  })
-});
+    hamburgerMenu.classList.toggle("header_menu");
+}
 
 
 
-// (function($){
-// 	$(function(){
 
 
+var slider = document.querySelector(".range")
+var values = document.querySelector(".value")
 
-//         // Phone nav click function
-//         $('.hamburger').click(function () {
-//             $("body").toggleClass("navShown");
-//             $(".nav-wrap").;
+values.textContent = slider.value;
+slider.oninput = function (){
+    values.textContent = this.value + "$";
+}
+
+var slideri = document.getElementById("rangei")
+var valuesi = document.getElementById("ui")
+
+valuesi.textContent = slideri.value;
+slideri.oninput = function (){
+    valuesi.textContent = this.value + "$";
+}
+
+var sliderii = document.getElementById("rangeii")
+var valuesii = document.getElementById("uii")
+
+valuesii.textContent = sliderii.value;
+sliderii.oninput = function (){
+    valuesii.textContent = this.value + "$";
+}
+
+
+// var x = document.getElementById("price_input").value;
+var y =document.querySelector(".value");
+// var o =document.querySelector(".range::-webkit-slider-thumb")
+// if (x == 10) {
+//     console.log("true")
+// }else{console.log("flase")}
+
+
+document.querySelector("#range").addEventListener("click",call);
+document.querySelector("#rangei").addEventListener("click",calli);
+document.querySelector("#rangeii").addEventListener("click",callii);
+
+function call() {
+//    window.history.back()
+// alert(location.href)
+// alert("helo")
+// if(confirm("are you sure"))
+    // location.href = "https://www.youtube.com/results?search_query=how+make+auto+maltiflu+calculator+javascript"
+ 
+    // var first = document.querySelector("#range").value 
+    document.querySelector("#first").style.display="flex"
+    document.querySelector(".secound").style.display="none"
+    document.querySelector(".threed").style.display="none"
+        
+        
+        }
+       
+        
+
+function calli() {
+        document.querySelector("#first").style.display="none"
+        document.querySelector(".secound").style.display="flex"
+        document.querySelector(".threed").style.display="none"
+        // var secound = document.querySelector("#rangei").value 
+  
+}
+    function callii() {
+        document.querySelector("#first").style.display="none"
+        document.querySelector(".secound").style.display="none"
+        document.querySelector(".threed").style.display="flex"
+            var threed = document.querySelector("#rangeii").value 
+        if(threed < "70000$"){
+           alert("hellow")
+        }else{
+            
+           alert("world")
+        }
+        
+        }
+            
+
